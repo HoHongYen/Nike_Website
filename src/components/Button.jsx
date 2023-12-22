d06b3aedb16f3e35d310def260380440574086f8
Ho Hong Yen <yenvt9a1@gmail.com>
2023-12-22 20:34:52 +0700

    Finish Subscribe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
     return (
         <button
             className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none  rounded-full 
                 ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : 'bg-coral-red border-coral-red text-white '}
+                ${fullWidth && 'w-full'}
             `} >
             {label}
             {iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />}
