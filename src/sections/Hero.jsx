24522d79a744fe7f01761a79e5db8b23141352e5
Ho Hong Yen <yenvt9a1@gmail.com>
2023-12-22 15:49:32 +0700

    Finish Hero, begin ShoeCard

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,46 @@
+import { arrowRight } from "../assets/icons"
+import { bigShoe1 } from "../assets/images"
+import Button from "../components/Button"
+import ShoeCard from "../components/ShoeCard"
+import { shoes, statistics } from "../constants"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">
+            Nike
+          </span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:Max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.</p>
+        <Button label="Show now" iconURL={arrowRight}  />
+
+        <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img src={bigShoe1} alt="Shoe collection" width={610} height={500} className="object-contain relative z-10"/>
+        <div>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   )
 }
 
